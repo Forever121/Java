b9681160cee19735148c96ab52d4da6fb6201f22
Forever <2307122700@qq.com>
2021-03-31 18:47:00 +0800

    2021.3.31 nigit update

--- ex7/src/ex7/ex7.java
+++ ex7/src/ex7/ex7.java
@@ -3,6 +3,7 @@ package ex7;
 public class ex7 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+    	System.out.println("10000以内的四个完全数为：");
         for (int n = 1; n <= 10000; n++) {
             int m = 0;
 
